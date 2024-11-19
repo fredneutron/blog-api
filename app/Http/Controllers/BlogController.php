982f250c29539f8c9fbd7b76ffc75d0b892ee8d0
fredneutron <adelabu4fred@gmail.com>
2024-11-19 17:58:28 +0100

    add Blog and Post Controllers

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Blog;
+use App\Http\Requests\BlogRequest;
+
+class BlogController extends Controller
+{
+    public function index()
+    {
+        $blogs = Blog::with('user')->get();
+        return response()->json($blogs);
+    }
+
+    public function store(BlogRequest $request)
+    {
+        $blog = Blog::create($request->validated());
+        return response()->json($blog, 201);
+    }
+
+    public function show(Blog $blog)
+    {
+        $blog->load('user', 'posts');
+        return response()->json($blog);
+    }
+
+    public function update(BlogRequest $request, Blog $blog)
+    {
+        $blog->update($request->validated());
+        return response()->json($blog);
+    }
+
+    public function destroy(Blog $blog)
+    {
+        $blog->delete();
+        return response()->json(['message' => 'Blog deleted successfully!'], 204);
+    }
+}
---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Models\Blog;
+use App\Http\Requests\PostRequest;
+
+class PostController extends Controller
+{
+    public function index(Blog $blog)
+    {
+        $posts = $blog->posts()->with('user')->get();
+        return response()->json($posts);
+    }
+
+    public function store(PostRequest $request, Blog $blog)
+    {
+        $post = $blog->posts()->create($request->validated());
+        return response()->json($post, 201);
+    }
+
+    public function show(Blog $blog, Post $post)
+    {
+        $post->load('user', 'likes', 'comments');
+        return response()->json($post);
+    }
+
+    public function update(PostRequest $request, Blog $blog, Post $post)
+    {
+        $post->update($request->validated());
+        return response()->json($post);
+    }
+
+    public function destroy(Blog $blog, Post $post)
+    {
+        $post->delete();
+        return response()->json(['message' => 'Post deleted successfully!'], 204);
+    }
+
+    public function like(Request $request, Post $post)
+    {
+        $post->likes()->create(['user_id' => $request->user_id]);
+        return response()->json(['message' => 'Post liked successfully']);
+    }
+
+    public function comment(Request $request, Post $post)
+    {
+        $comment = $post->comments()->create([
+            'content' => $request->content,
+            'user_id' => $request->user_id
+        ]);
+        return response()->json($comment, 201);
+    }
+}
